8a8f0b1991f5607d04ce5c7eda9b912e819ac5e6
elberdev <elberdev@gmail.com>
2016-02-01 20:53:14 -0500

    change micropost model to validate for minimum amount of content (cannot be blank)

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,7 @@
 class Micropost < ActiveRecord::Base
   belongs_to :user
-  validates :content, length: { maximum: 140 }
+
+  # validates that the maximum length of the comments is 140 characters
+  # and that the minimum input is 1 character (cannot be empty)
+  validates :content, length: { maximum: 140 }, presence: true
 end
